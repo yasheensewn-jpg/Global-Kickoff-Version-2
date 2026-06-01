648c1f5bb719a9e795613dc98aad6d27b20c974b
yasheensewn-jpg <yasheen.sewn@gmail.com>
2026-06-01 19:44:41 +0930

    Integrate Meta Facebook SDK across Web, iOS, and Android

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Capacitor
+import FBSDKCoreKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -8,6 +9,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        ApplicationDelegate.shared.application(
+            application,
+            didFinishLaunchingWithOptions: launchOptions
+        )
         return true
     }
 
